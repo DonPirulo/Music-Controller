b523b98d8ab2c15afc96838e432b094bf5a4ec70
Pirulo <aepaulo@gmail.com>
2021-05-14 02:04:10 -0300

    first commit

---
+++ music_controller/frontend/src/components/app.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import HomePage from './HomePage';
+import JoinRoomPage from './JoinRoomPage';
+import CreateRoomPage from './CreateRoomPage'
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (<div className="center">
+            <HomePage/>
+        </div>
+        );
+    }
+}
+
+
+const appDiv = document.getElementById("app");
+
+render(<App />, appDiv);
